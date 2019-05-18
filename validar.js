dfbdaf66d10f4681b427684230236c159e27ae93
tatcjho <tatycjho@gmail.com>
2019-05-18 17:07:27 -0500

    Funcion para validar la fecha ddmmyyyy

--- validar.js
+++ validar.js
@@ -59,3 +59,41 @@ function soloNumeros(elemento){
     }
 
 }
+
+
+function ValidarFecha(){
+    // Almacenamos el valor digitado en TxtFecha
+    var Fecha = document.getElementById('fechaNac').value;
+    var Mensaje = '';
+     document.getElementById('Mensaje').className = '';
+    
+    // Si la fecha está completa comenzamos la validación
+    if(Fecha.length == 8){
+
+        var Anio = Fecha.substr(4, 4); // Extraemos en año
+        var Mes = parseFloat(Fecha.substr(2, 2)) - 1; // Extraemos el mes
+        var Dia = Fecha.substr(0, 2); // Extraemos el día
+    
+     //var Anio = Fecha.substr(0, 4); // Extraemos en año
+     //var Mes = parseFloat(Fecha.substr(4, 2)) - 1; // Extraemos el mes
+     //var Dia = Fecha.substr(6, 2); // Extraemos el día
+     
+     // Con la función Date() de javascript evaluamos si la fecha existe
+     var VFecha = new Date(Anio, Mes, Dia);
+     
+     // Si las partes de la fecha concuerdan con las que digitamos, es correcta
+     if((VFecha.getFullYear() == Anio) && (VFecha.getMonth() == Mes) && (VFecha.getDate() == Dia)){
+      Mensaje = 'Fecha correcta';
+     document.getElementById('Mensaje').className = 'Mensaje1';
+     }
+     else{
+      Mensaje = 'Fecha incorrecta';
+     document.getElementById('Mensaje').className = 'Mensaje';
+     }
+    }
+    
+    // Mostramos el mesaje
+    document.getElementById('Mensaje').innerHTML = Mensaje;
+}
+   
+   
