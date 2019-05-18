4462a260690c652c767eaa3c2bbdfaca5256eec9
tatcjho <tatycjho@gmail.com>
2019-05-18 12:30:49 -0500

    Funcion para que solo se valide el ingreso de texto y no numeros

--- validar.js
+++ validar.js
@@ -13,3 +13,26 @@ function validar(){
     }
 
 }
+
+function sololetras(elemento) {
+    key=elemento.keyCode || elemento.which;
+
+    teclado=String.fromCharCode(key).toLowerCase();
+
+    letras="qwertyuiopasdfghjklñzxcvbnm ";
+
+    especiales="8-37-38-46-164";
+
+    teclado_especial=false;
+
+    for(var i in especiales){
+        if(key==especiales[i]){
+            teclado_especial=true;
+            break;
+        }
+    }
+
+    if(letras.indexOf(teclado)==-1 && !teclado_especial){
+        return false;
+    }
+}
