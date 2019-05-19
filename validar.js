56fc3ce1d741ba9a13c60b08be0a691463516477
tatcjho <tatycjho@gmail.com>
2019-05-19 13:30:20 -0500

    Funcion validar cedula del Ecuador

--- validar.js
+++ validar.js
@@ -166,6 +166,45 @@ function ValidarMail(email){
 }
 
 
+//FUNCION VALIDAR CEDULA ECUADOR 
+
+    
+function cedulaEC(cedula) {
+    object=document.getElementById(cedula);
+    var cad = document.getElementById("cedula").value.trim();
+    var total = 0;
+    var longitud = cad.length;
+    var longcheck = longitud - 1;
+
+    if (cad !== "" && longitud === 10){
+      for(i = 0; i < longcheck; i++){
+        if (i%2 === 0) {
+          var aux = cad.charAt(i) * 2;
+          if (aux > 9) aux -= 9;
+          total += aux;
+        } else {
+          total += parseInt(cad.charAt(i)); // parseInt o concatenará en lugar de sumar
+        }
+      }
+
+      total = total % 10 ? 10 - total % 10 : 0;
+
+      if (cad.charAt(longitud-1) == total) {
+       // document.getElementById("salida").innerHTML = ("Cedula Válida");
+       object.style.color="#000"; 
+       return;
+
+      }else{
+
+        object.style.color="#f00";
+      }
+    }
+  }
+
+
+
+
+
 
 
   
