9d8011fe751b8e1d8ca09414818d7fc4b7f9804c
tatcjho <tatycjho@gmail.com>
2019-05-20 11:21:41 -0500

    Funciones

---
+++ Calculadora/calcula.js
@@ -0,0 +1,25 @@
+//Funcion para validar que solo se acepte numeros 
+
+function soloNumeros(e){
+
+    key =e.keyCode || e.which; //entrada del teclado
+    teclado=String.fromCharCode(key);
+
+    numeros="0123456789";
+     
+    especiales="8-37-39-46";
+
+    tecla_especial=false;
+
+    for(var i in especiales){
+        if(key==especiales[i]){
+            tecla_especial=true;
+        }
+    }
+    if(numeros.indexOf(teclado)==-1 && !tecla_especial){
+        return false;
+    }
+}
+
+
+
